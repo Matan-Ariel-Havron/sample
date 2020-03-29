ee176f7743443fb6301ab6e5e29c5780b523cd76
Matan Ariel-Havron <matan.arielhavron1@gmail.com>
2020-03-29 16:42:32 +0300

    second commit

--- demo.py
+++ demo.py
@@ -4,14 +4,10 @@ from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-
 
 def main():
     r = requests.get("https://coreyms.com")
     print(r.status_code)
-    print("sdf")
 
 
 if __name__ == "__main__":
